27269be9f05971ac0766f154d8f9457fe69b0533
John Knudson <jknudson@umich.edu>
2017-05-17 14:16:03 -0700

    added Vundle installation to setup_dotfiles.sh

--- setup_dotfiles.sh
+++ setup_dotfiles.sh
@@ -1,8 +1,18 @@
+#!/bin/bash
 DOTFILES=(`ls -a | grep "^\.[[:alpha:]]" | grep -v ^\.git$`)
 DOTDIR=`pwd`
+VUNDLEREPO="https://github.com/VundleVim/Vundle.vim.git"
+VUNDLELOCATION="~/.vim/bundle/Vundle.vim"
+eval VUNDLELOCATION="$VUNDLELOCATION"
+
 for f in ${DOTFILES[@]}
 do
 	echo "removing $f from home and linking to dotfiles"
-	rm ~/$f
+	rm -f ~/$f
 	ln -s $DOTDIR/$f ~/$f
 done
+
+if [ ! -d $VUNDLELOCATION ]; then
+	mkdir -p $VUNDLELOCATION
+	git clone $VUNDLEREPO $VUNDLELOCATION
+fi
